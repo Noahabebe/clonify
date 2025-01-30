6d47000a976b03c4c995b5372fc4c572feca2626
Noah Abebe <73600593+Noahabebe@users.noreply.github.com>
2025-01-30 08:07:57 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,3 +3,4 @@
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
 mv ffmpeg-*-static/ffmpeg /usr/local/bin/
 pip install -r requirements.txt
+sudo apt update && sudo apt install -y libgl1-mesa-glx
