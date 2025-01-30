6773742d389f477043b9ca62500616bcde24c068
Noah Abebe <73600593+Noahabebe@users.noreply.github.com>
2025-01-30 07:45:44 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,3 +2,4 @@
 # Update package lists and install ffmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
 mv ffmpeg-*-static/ffmpeg /usr/local/bin/
+pip install -r requirements.txt
